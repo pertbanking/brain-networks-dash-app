297cf0f235be181bafcf314a2472f2415027f6ff
Josh Petrin <joshua.m.petrin@vanderbilt.edu>
2022-07-13 22:56:32 -0400

    q going to bed lol

--- app.py
+++ app.py
@@ -13,3 +13,21 @@ import pandas as pd
 from data.load import brain_data
 
 
+# first, do some data inspection
+# it's hard to not make assumptions on this data ;P
+network_and_nodes = brain_data.loc[0].to_frame().reset_index()
+network_and_hemi  = brain_data.loc[1].to_frame().reset_index()
+
+magnitudes = brain_data.loc[3:].reset_index().set_index('network').head()
+magnitudes = magnitudes.drop(columns='index')
+
+num_trials = len(magnitudes.index)
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Slider(-5, 10, 1, value=-3)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
