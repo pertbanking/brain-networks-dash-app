fc49a90da34bdf3a477e182551b2ff1da9d656d1
Josh Petrin <joshua.m.petrin@vanderbilt.edu>
2022-07-13 22:31:44 -0400

    initial dash app

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# core imports
+# import whatevah
+
+# import Dash
+from dash import Dash, html, dcc
+
+# import Pandas
+import pandas as pd
+
+# retrieve all of our data
+from data.load import brain_data
+
+
